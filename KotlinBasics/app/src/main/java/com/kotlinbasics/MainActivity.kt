6030a26e841e804f35b25afec44c6d8e1ef0c8eb
hongdaeun11 <sally20114@gmail.com>
2025-09-12 15:30:30 +0900

    week02 : README.md & variables v0.2

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -27,7 +27,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        week02Variables()
     }
+}
+
+fun week02Variables(){
+    println("Week02 Variables")
+
+    val courseName = "Mobile Programming"
+    //courseName = "IoT Programming"
+    var week = 1
+    week = 2
+
+
+    println("Course : $courseName")
+    println("Week : $week")
 }
 
 @Composable
---
+++ README.md
@@ -0,0 +1,2 @@
+This remote repository manages code materials related to the Mobile Programming coursefor the Fall 2025 semester.
+
