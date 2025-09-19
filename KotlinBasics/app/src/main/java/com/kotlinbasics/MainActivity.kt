2e988da1b3b850b645d85c7fa31fe9eeb30a5b74
hongdaeun11 <sally20114@gmail.com>
2025-09-19 15:06:17 +0900

    week03 : class v0.1

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlinbasics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -27,15 +28,18 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        week02Variables()
-        week02Functions()
+//        week02Variables()
+//        week02Functions()
+        week03Classes()
+
     }
 }
 
 
 
 
-fun week02Variables(){
+
+private fun week02Variables(){
 //    println("Week02 Variables")
 //
 //    val courseName = "Mobile Programming"
@@ -60,7 +64,7 @@ fun week02Variables(){
 
     println("Age : $age, Height : $height, Student : $isStudent")
 }
-fun week02Functions() {
+private fun week02Functions() {
     println("Week02 Functions")
 
 //    fun greet(name: String) = "Hello, $name!"
@@ -81,12 +85,39 @@ fun week02Functions() {
         //두번째 인자를 세팅해주지 않으면 기본적으로 19로 지정됨
         println("My name is $name and I'm $age years old")
     }
-//    println(greet(name:"Kotlin"))
-//    println("Sum : ${add(a:5, b:-71)}")
-//    introduce(name:"Park")
-//    introduce(name:"Kim", age:29)
+    println(greet(name = "Kotlin"))
+    println("Sum : ${add(a = 5, b = -71)}")
+    introduce(name = "Park")
+    introduce(name = "Kim", age = 29)
 
 }
+
+
+private fun week03Classes(){
+    println("== Kotlin Classes ==")
+
+    class Student{
+        var name: String = ""
+        var age: Int = 0
+
+        fun introduce(){
+            println("Hi, I'm $name and I'm $age years old")
+        }
+    }
+    val student = Student()  //기본생성자 호출
+    student.name = "Choi"
+    student.age = 22
+    student.introduce()
+
+    data class Person(val name: String, val age: Int)
+
+    val person1 = Person(name="Lee", age=24)
+    val person2 = Person(name="Lee", age=24)
+
+    println("Person1: $person1")
+    println("Equal?: ${person1 == person2}")
+}
+
 
 
 @Composable
